f441ca8cff07e004428e4269ede2183b19e31410
LWIZN <tony23068527@gmail.com>
2022-07-24 08:54:41 +0800

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Container = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 3px;
+  padding: 8px;
+`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Item = styled.div`
+  padding: 8px;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+  margin-bottom: 8px;
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: "Learn React",
+        },
+        {
+          id: 2,
+          text: "Learn Redux",
+        },
+        {
+          id: 3,
+          text: "Watch TV",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <DragDropContext
+        onDragEnd={(result) => {
+          console.log(result);
+          const { source, destination, draggableId } = result;
+          if (!destination) {
+            return;
+          }
+
+          let arr = Array.from(this.state.todos);
+          console.log(arr);
+          const [remove] = arr.splice(source.index, 1);
+          arr.splice(destination.index, 0, remove);
+          this.setState({
+            todos: arr,
+          });
+        }}
+      >
+        <Droppable droppableId="d">
+          {(provided) => (
+            <Container ref={provided.innerRef} {...provided.droppableProps}>
+              {this.state.todos.map((t, i) => (
+                <Draggable draggableId={t.id} index={i}>
+                  {(p) => (
+                    <Item
+                      ref={p.innerRef}
+                      {...p.draggableProps}
+                      {...p.dragHandleProps}
+                      key={t.id}
+                    >
+                      {t.text}
+                    </Item>
+                  )}
+                </Draggable>
+              ))}
+            </Container>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
